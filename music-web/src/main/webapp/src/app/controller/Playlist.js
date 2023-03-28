5591df5ffdf4df5acf75d976eed8fe22d3e74b98
Dayitva <dayitvagoel@gmail.com>
2023-03-28 16:49:46 +0530

    add: feature 3

--- music-web/src/main/webapp/src/app/controller/Playlist.js
+++ music-web/src/main/webapp/src/app/controller/Playlist.js
@@ -1,84 +1,180 @@
-'use strict';
+"use strict";
 
 /**
  * Playlist controller.
  */
-angular.module('music').controller('Playlist', function($scope, $state, $stateParams, Restangular, Playlist, NamedPlaylist) {
-  // Load playlist
-  Restangular.one('playlist', $stateParams.id).get().then(function(data) {
-    $scope.playlist = data;
-  });
+angular
+  .module("music")
+  .controller(
+    "Playlist",
+    function (
+      $scope,
+      $state,
+      $stateParams,
+      $http,
+      Restangular,
+      Playlist,
+      NamedPlaylist
+    ) {
+      // Load playlist
+      Restangular.one("playlist", $stateParams.id)
+        .get()
+        .then(function (data) {
+          $scope.playlist = data;
+        });
 
-  // Play a single track
-  $scope.playTrack = function(track) {
-    Playlist.removeAndPlay(track);
-  };
+      $scope.recommendLastFM = function () {
+        var tracks = new Array();
 
-  // Add a single track to the playlist
-  $scope.addTrack = function(track) {
-    Playlist.add(track, false);
-  };
+        for (var i = 0; i < $scope.playlist.tracks.length; i++) {
+          var site =
+            `https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${String($scope.playlist.tracks[i].artist.name)}&track=${String($scope.playlist.tracks[i].title)}&api_key=6e04e3362ef7be553491b6556935a61b&format=json`;
 
-  // Add all tracks to the playlist in a random order
-  $scope.shuffleAllTracks = function() {
-    Playlist.addAll(_.shuffle(_.pluck($scope.playlist.tracks, 'id')), false);
-  };
+          console.log(site);
 
-  // Play all tracks
-  $scope.playAllTracks = function() {
-    Playlist.removeAndPlayAll(_.pluck($scope.playlist.tracks, 'id'));
-  };
+          $http.get(site).then(
+            function (data) {
+              console.log(data);
+              tracks.push(data.data.similartracks.track);
+            },
+            function (error) {
+              console.log(error);
+            }
+          );
+        }
 
-  // Add all tracks to the playlist
-  $scope.addAllTracks = function() {
-    Playlist.addAll(_.pluck($scope.playlist.tracks, 'id'), false);
-  };
+        $scope.tracks = tracks;
+      };
 
-  // Like/unlike a track
-  $scope.toggleLikeTrack = function(track) {
-    Playlist.likeById(track.id, !track.liked);
-  };
+      // var spotify_api_site;
+      // var lastfm_api_site;
 
-  // Remove a track
-  $scope.removeTrack = function(order) {
-    NamedPlaylist.removeTrack($scope.playlist, order).then(function(data) {
-      $scope.playlist = data;
-    });
-  };
+      // spotify_api_site = "https://api.spotify.com/v1/search?q=" + query + "&type=track&market=IN&limit=5";
+      // lastfm_api_site = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + query + "&api_key=6e04e3362ef7be553491b6556935a61b&format=json";
 
-  // Delete the playlist
-  $scope.remove = function() {
-    NamedPlaylist.remove($scope.playlist).then(function() {
-      $state.go('main.default');
-    });
-  };
+      $scope.recommendSpotify = function () {
+        var tracks = new Array();
+        var rsite = "https://api.spotify.com/v1/recommendations?seed_tracks=";
 
-  // Update UI on track liked
-  $scope.$on('track.liked', function(e, trackId, liked) {
-    var track = _.findWhere($scope.playlist.tracks, { id: trackId });
-    if (track) {
-      track.liked = liked;
-    }
-  });
+        var heads = {authorization: "Bearer BQAvLNJZM2duEPtTSoaYdpEgtCeYtWgbhCED8N9IHvxSyqE_QDqakmZaG_NJNXjBQoMQ5z8zAB0uCX9BH-LP2EyBFZyK2puKcuCWgPp00YaZtzVceGsE2MN96Bxqc2PuGHHZWyBpW3W-Gtl9_bDhbSvxO0qP1sqnPc_Q7ifp49u49DW-u8A994nHDKe3hTJV_J9oWn1wHCgU1Ktm5DN1U06Zit-pFTgswxr44q2I1osO5U_fFyePr3puib23MlxOX22shSiRVl42lZSR1o7UIOKPdb_WYBaTFaQ3gWdUhvz213M_GiPa1A58myuS3EuoAS0_SrLQi1ZT2Tz27kg6AaPpGAWKqlxWZ1XuU7e8AZWjqwE"}
 
-  // Configuration for track sorting
-  $scope.trackSortableOptions = {
-    forceHelperSize: true,
-    forcePlaceholderSize: true,
-    tolerance: 'pointer',
-    handle: '.handle',
-    containment: 'parent',
-    helper: function(e, ui) {
-      ui.children().each(function() {
-        $(this).width($(this).width());
+        for (var i = 0; i < $scope.playlist.tracks.length; i++) {
+          var site = "https://api.spotify.com/v1/search?q=" + $scope.playlist.tracks[i].title + "&type=track&market=IN&limit=5";
+      
+          console.log(site);
+
+          $http.get(site, {headers: heads}).then(
+            function (data) {
+              console.log(data.data.tracks.items[0].id);
+              tracks.push(String(data.data.tracks.items[0].id));
+            },
+            function (error) {
+              console.log(error);
+            }
+          );
+        }
+
+        console.log(tracks)
+
+        
+        for(var i = 0; i < tracks.length; i++) {
+          console.log(tracks[i]);
+          rsite +=  "," + tracks[i];
+        }
+
+        // sleep(2000);
+
+        console.log(rsite);
+
+        $http.get(rsite, {headers: heads}).then(
+          function (data) {
+            console.log(data.data.track);
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+
+      };
+
+      // Play a single track
+      $scope.playTrack = function (track) {
+        Playlist.removeAndPlay(track);
+      };
+
+      // Add a single track to the playlist
+      $scope.addTrack = function (track) {
+        Playlist.add(track, false);
+      };
+
+      // Add all tracks to the playlist in a random order
+      $scope.shuffleAllTracks = function () {
+        Playlist.addAll(
+          _.shuffle(_.pluck($scope.playlist.tracks, "id")),
+          false
+        );
+      };
+
+      // Play all tracks
+      $scope.playAllTracks = function () {
+        Playlist.removeAndPlayAll(_.pluck($scope.playlist.tracks, "id"));
+      };
+
+      // Add all tracks to the playlist
+      $scope.addAllTracks = function () {
+        Playlist.addAll(_.pluck($scope.playlist.tracks, "id"), false);
+      };
+
+      // Like/unlike a track
+      $scope.toggleLikeTrack = function (track) {
+        Playlist.likeById(track.id, !track.liked);
+      };
+
+      // Remove a track
+      $scope.removeTrack = function (order) {
+        NamedPlaylist.removeTrack($scope.playlist, order).then(function (data) {
+          $scope.playlist = data;
+        });
+      };
+
+      // Delete the playlist
+      $scope.remove = function () {
+        NamedPlaylist.remove($scope.playlist).then(function () {
+          $state.go("main.default");
+        });
+      };
+
+      // Update UI on track liked
+      $scope.$on("track.liked", function (e, trackId, liked) {
+        var track = _.findWhere($scope.playlist.tracks, { id: trackId });
+        if (track) {
+          track.liked = liked;
+        }
       });
-      return ui;
-    },
-    stop: function (e, ui) {
-      // Send new positions to server
-      $scope.$apply(function () {
-        NamedPlaylist.moveTrack($scope.playlist, ui.item.attr('data-order'), ui.item.index());
-      });
+
+      // Configuration for track sorting
+      $scope.trackSortableOptions = {
+        forceHelperSize: true,
+        forcePlaceholderSize: true,
+        tolerance: "pointer",
+        handle: ".handle",
+        containment: "parent",
+        helper: function (e, ui) {
+          ui.children().each(function () {
+            $(this).width($(this).width());
+          });
+          return ui;
+        },
+        stop: function (e, ui) {
+          // Send new positions to server
+          $scope.$apply(function () {
+            NamedPlaylist.moveTrack(
+              $scope.playlist,
+              ui.item.attr("data-order"),
+              ui.item.index()
+            );
+          });
+        },
+      };
     }
-  };
-});
+  );
--- music-web/src/main/webapp/src/app/controller/Search.js
+++ music-web/src/main/webapp/src/app/controller/Search.js
@@ -1,63 +1,122 @@
-'use strict';
+"use strict";
 
 /**
  * Search controller.
  */
-angular.module('music').controller('Search', function($rootScope, $scope, $stateParams, Restangular, Playlist) {
-  // Server call debounced
-  var search = _.debounce(function(query) {
-    Restangular.one('search', query).get({ limit: 100 }).then(function(data) {
-      $scope.results = data;
-    });
-  }, 300);
+angular
+  .module("music")
+  .controller(
+    "Search",
+    function ($rootScope, $scope, $stateParams, $http, Restangular, Playlist) {
+      // Server call debounced
+      var spotify_api_site;
+      var lastfm_api_site;
 
-  // Watch the search query
-  $scope.$watch('$stateParams.query', function(newval) {
-    search(newval);
-  });
+      var search = _.debounce(function (query) {
+        spotify_api_site = "https://api.spotify.com/v1/search?q=" + query + "&type=track&market=IN&limit=5";
+        lastfm_api_site = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + query + "&api_key=6e04e3362ef7be553491b6556935a61b&format=json";
+        
+        Restangular.one("search", query)
+          .get({ limit: 100 })
+          .then(function (data) {
+            $scope.results = data;
+          });
+      }, 300);
 
-  // Play a single track
-  $scope.playTrack = function(track) {
-    Playlist.removeAndPlay(track);
-  };
+      
+      $scope.searchSpotify = function () {
+        console.log(spotify_api_site);
+        var heads = {authorization: "Bearer BQAvLNJZM2duEPtTSoaYdpEgtCeYtWgbhCED8N9IHvxSyqE_QDqakmZaG_NJNXjBQoMQ5z8zAB0uCX9BH-LP2EyBFZyK2puKcuCWgPp00YaZtzVceGsE2MN96Bxqc2PuGHHZWyBpW3W-Gtl9_bDhbSvxO0qP1sqnPc_Q7ifp49u49DW-u8A994nHDKe3hTJV_J9oWn1wHCgU1Ktm5DN1U06Zit-pFTgswxr44q2I1osO5U_fFyePr3puib23MlxOX22shSiRVl42lZSR1o7UIOKPdb_WYBaTFaQ3gWdUhvz213M_GiPa1A58myuS3EuoAS0_SrLQi1ZT2Tz27kg6AaPpGAWKqlxWZ1XuU7e8AZWjqwE"};
+        $http.get(spotify_api_site, {headers: heads}).then(
+          function (data) {
+            console.log(data.data.tracks.items);
+            $scope.tracks = data.data.tracks.items;
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      };
 
-  // Add a single track to the playlist
-  $scope.addTrack = function(track) {
-    Playlist.add(track, false);
-  };
+      $scope.searchLastFM = function () {
+        console.log(lastfm_api_site);
+        $http.get(lastfm_api_site).then(
+          function (data) {
+            console.log(data.data.results.trackmatches.track);
+            $scope.track = data.data.results.trackmatches.track;
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      };
 
-  // Like/unlike a track
-  $scope.toggleLikeTrack = function(track) {
-    Playlist.likeById(track.id, !track.liked);
-  };
+      $scope.recommendLastFM = function () {
+        var site = "https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=cher&track=believe&limit=10&api_key=6e04e3362ef7be553491b6556935a61b&format=json"
+        console.log(site);
+        $http.get(site).then(
+          function (data) {
+            console.log(data.data.similartracks.track);
+            $scope.track_name = data.data.similartracks.track.name;
+            $scope.artist_name = data.data.similartracks.track.artist.name;
+            $scope.duration = data.data.similartracks.track.duration;
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      };
 
-  // Update UI on track liked
-  $scope.$on('track.liked', function(e, trackId, liked) {
-    var track = _.findWhere($scope.results.tracks, { id: trackId });
-    if (track) {
-      track.liked = liked;
+      // Watch the search query
+      $scope.$watch("$stateParams.query", function (newval) {
+        search(newval);
+      });
+
+      // Play a single track
+      $scope.playTrack = function (track) {
+        Playlist.removeAndPlay(track);
+      };
+
+      // Add a single track to the playlist
+      $scope.addTrack = function (track) {
+        Playlist.add(track, false);
+      };
+
+      // Like/unlike a track
+      $scope.toggleLikeTrack = function (track) {
+        Playlist.likeById(track.id, !track.liked);
+      };
+
+      // Update UI on track liked
+      $scope.$on("track.liked", function (e, trackId, liked) {
+        var track = _.findWhere($scope.results.tracks, { id: trackId });
+        if (track) {
+          track.liked = liked;
+        }
+      });
+
+      // Add all tracks to the playlist in a random order
+      $scope.shuffleAllTracks = function () {
+        Playlist.addAll(_.shuffle(_.pluck($scope.results.tracks, "id")), false);
+      };
+
+      // Play all tracks
+      $scope.playAllTracks = function () {
+        Playlist.removeAndPlayAll(_.pluck($scope.results.tracks, "id"));
+      };
+
+      // Add all tracks to the playlist
+      $scope.addAllTracks = function () {
+        Playlist.addAll(_.pluck($scope.results.tracks, "id"), false);
+      };
+
+      // Load and play an album
+      $scope.playAlbum = function (album) {
+        Restangular.one("album", album.id)
+          .get()
+          .then(function (data) {
+            Playlist.removeAndPlayAll(_.pluck(data.tracks, "id"));
+          });
+      };
     }
-  });
-
-  // Add all tracks to the playlist in a random order
-  $scope.shuffleAllTracks = function() {
-    Playlist.addAll(_.shuffle(_.pluck($scope.results.tracks, 'id')), false);
-  };
-
-  // Play all tracks
-  $scope.playAllTracks = function() {
-    Playlist.removeAndPlayAll(_.pluck($scope.results.tracks, 'id'));
-  };
-
-  // Add all tracks to the playlist
-  $scope.addAllTracks = function() {
-    Playlist.addAll(_.pluck($scope.results.tracks, 'id'), false);
-  };
-
-  // Load and play an album
-  $scope.playAlbum = function(album) {
-    Restangular.one('album', album.id).get().then(function(data) {
-      Playlist.removeAndPlayAll(_.pluck(data.tracks, 'id'));
-    });
-  };
-});
+  );
